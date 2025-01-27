aaa0e086b6ab804e64c99b5fefec1c1b1c151ac1
Brandon Burkett <brandonjeffreyburkett@gmail.com>
2025-01-26 17:36:41 -0800

    Initial Commit 1/26/25

---
+++ javafx-game/src/main/java/io/github/branburkett/DemoController.java
@@ -0,0 +1,49 @@
+package io.github.branburkett;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+//import javafx.scene.control.TextField;
+import com.gluonhq.charm.glisten.control.TextField;
+
+
+public class DemoController {
+
+    @FXML
+    private Button demoButton;
+    @FXML
+    private com.gluonhq.charm.glisten.control.TextField demoTextField;
+
+    @FXML
+    private Label demoLabel;
+    @FXML
+    private CheckBox demoCheckBox;
+    @FXML
+    private Slider demoSlider;
+
+    public void initialize() {
+        demoSlider.setValue(50); // Set default slider value
+    }
+
+    @FXML
+    public void handleButtonAction() {
+        System.out.println("TextField Value: " + demoTextField.getText());
+        demoLabel.setText("Hello, " + demoTextField.getText());
+    }
+
+    @FXML
+    public void handleCheckBoxAction() {
+        if (demoCheckBox.isSelected()) {
+            demoLabel.setText("Checkbox is selected");
+        } else {
+            demoLabel.setText("Checkbox is not selected");
+        }
+    }
+
+    @FXML
+    public void handleSliderAction() {
+        demoLabel.setText("Slider Value: " + (int) demoSlider.getValue());
+    }
+}
---
+++ javafx-game/src/main/java/io/github/branburkett/Main.java
@@ -0,0 +1,40 @@
+package io.github.branburkett;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import java.net.URL;
+
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Only declare the root and scene once
+        URL fxmlUrl = getClass().getResource("/demo.fxml");
+        if (fxmlUrl == null) {
+            System.err.println("FXML file not found!");
+            System.exit(0);
+        }
+        else {
+            System.err.println("FXML file found!");
+        }
+ 
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/demo.fxml"));
+        Parent root = loader.load();  // Only declare once
+        Scene scene = new Scene(root); // Only declare once
+
+        primaryStage.setTitle("JavaFX Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
