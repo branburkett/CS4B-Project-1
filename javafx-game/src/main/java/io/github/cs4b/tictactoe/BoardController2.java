067802c0ab53f6af08fbbf8ccf66578634e9f0ec
Brandon Burkett <brandonjeffreyburkett@gmail.com>
2025-01-31 18:04:48 -0800

    Made changes to loading screen and single player board.

--- javafx-game/src/main/java/io/github/cs4b/tictactoe/BoardController2.java
+++ javafx-game/src/main/java/io/github/cs4b/tictactoe/BoardController2.java
@@ -3,6 +3,7 @@ package io.github.cs4b.tictactoe;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class BoardController2 {
 
@@ -10,7 +11,44 @@ public class BoardController2 {
     private AnchorPane anchorPane;
 
     @FXML
+    private GridPane gamePane;
+
+    @FXML
     private Button startButton;
 
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Button button6;
+
+    @FXML
+    private Button button7;
+
+    @FXML
+    private Button button8;
+
+    @FXML
+    private Button button9;
+
+    @FXML
+    private Button newGame;
+
     // Add any other necessary fields and methods
+    @FXML
+    private void clickButton1() {
+        System.out.println("Button 1 pressed");
+    }
 }
